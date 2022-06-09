dbf5594ffb0dc6484ad97590ad6e8a111ad94234
Aleksei <kilroygood@gmail.com>
2022-06-09 19:13:02 +0300

    AddHardhatAndCode

---
+++ contracts/Greeter.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract Auction {
+
+mapping (address => uint) public mappAuc;
+mapping (address => uint) public playersValue;
+
+struct AuctionSt {  
+    address bettor;
+}
+
+
+AuctionSt[] public auctions;
+
+
+function addBet() payable public {
+    address addr = msg.sender;
+
+    AuctionSt memory newAuction = AuctionSt({
+    bettor: addr
+});
+
+auctions.push(newAuction);
+
+addBetter();
+}
+
+uint public TotalValue;
+uint public LastBet;
+
+function addBetter() payable public {
+
+    require(msg.value >= 1000000000000000, "Minimum bet 0.01 Ether!");
+    require(msg.value > LastBet, "Your bid must be greater than the previous one!");
+    LastBet = msg.value;
+    
+    uint counter = mappAuc[msg.sender];
+    require(counter < 3, "You spent all your shants!");
+
+    
+    uint payad = playersValue[msg.sender];
+    uint totalPayPersom = payad + msg.value;
+    
+    counter = counter + 1;
+    mappAuc[msg.sender] = counter;
+
+
+    playersValue[msg.sender] = totalPayPersom;
+    TotalValue = TotalValue + msg.value;
+
+
+}
+
+
+
+function mainAuc() payable public {
+}
+
+}
